55e91188932da75f8c2ef9beab9ec124efe86343
Don Espe <Don.Espe@gmail.com>
2024-05-09 16:19:15 -0500

    Initial Commit

---
+++ RiverSimulator/RiverSimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiverSimulatorApp.swift
+//  RiverSimulator
+//
+//  Created by Don Espe on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct RiverSimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
